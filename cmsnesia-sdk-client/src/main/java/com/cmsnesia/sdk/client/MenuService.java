7256bfbe2c8e334d265fd3c4b9fcee2bbdad21cd
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-02-17 18:53:07 +0700

    Fixes for sdk client

--- cmsnesia-sdk-client/src/main/java/com/cmsnesia/sdk/client/MenuService.java
+++ cmsnesia-sdk-client/src/main/java/com/cmsnesia/sdk/client/MenuService.java
@@ -1,9 +1,8 @@
 package com.cmsnesia.sdk.client;
 
-import com.cmsnesia.model.CategoryDto;
+import com.cmsnesia.model.MenuDto;
 import com.cmsnesia.model.api.Result;
 import com.cmsnesia.model.request.IdRequest;
-import com.cmsnesia.model.request.NameRequest;
 import com.cmsnesia.sdk.client.domain.Page;
 import feign.Headers;
 import feign.Param;
@@ -14,18 +13,18 @@ public interface MenuService {
 
   @RequestLine("GET /menu/find?page={page}&size={size}")
   @Headers("Content-Type: application/json")
-  Mono<Page<CategoryDto>> find(
-      CategoryDto categoryDto, @Param("page") Integer page, @Param("size") Integer size);
+  Mono<Page<MenuDto>> find(
+      MenuDto menuDto, @Param("page") Integer page, @Param("size") Integer size);
 
   @RequestLine("POST /menu/add")
   @Headers("Content-Type: application/json")
-  Mono<Result<CategoryDto>> add(NameRequest nameRequest);
+  Mono<Result<MenuDto>> add(MenuDto menuDto);
 
   @RequestLine("PUT /menu/edit")
   @Headers("Content-Type: application/json")
-  Mono<Result<CategoryDto>> edit(CategoryDto categoryDto);
+  Mono<Result<MenuDto>> edit(MenuDto menuDto);
 
   @RequestLine("PUT /menu/delete")
   @Headers("Content-Type: application/json")
-  Mono<Result<CategoryDto>> delete(IdRequest idRequest);
+  Mono<Result<MenuDto>> delete(IdRequest idRequest);
 }
