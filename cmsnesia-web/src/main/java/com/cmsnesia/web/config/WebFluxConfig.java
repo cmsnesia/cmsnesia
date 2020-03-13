c22db8c5370d5334470bea0797e4ae84158b22fe
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-03-13 18:44:01 +0700

    Update config

--- cmsnesia-web/src/main/java/com/cmsnesia/web/config/WebFluxConfig.java
+++ cmsnesia-web/src/main/java/com/cmsnesia/web/config/WebFluxConfig.java
@@ -2,7 +2,6 @@ package com.cmsnesia.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -19,14 +18,6 @@ import reactor.core.publisher.Mono;
 @EnableWebFlux
 @Configuration
 public class WebFluxConfig implements WebFluxConfigurer {
-
-  private static final String ALLOWED_HEADERS = "*";
-  private static final String ALLOWED_EXPOSED_HEADERS = "*";
-  private static final String ALLOWED_REQUEST_HEADERS = "*";
-  private static final String ALLOWED_METHODS = "*";
-  private static final String ALLOWED_ORIGIN = "*";
-  private static final String ALLOWED_CREDENTIALS = "true";
-  private static final String MAX_AGE = "3600";
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -45,22 +36,6 @@ public class WebFluxConfig implements WebFluxConfigurer {
       ServerHttpRequest request = ctx.getRequest();
       if (CorsUtils.isCorsRequest(request)) {
         ServerHttpResponse response = ctx.getResponse();
-        HttpHeaders headers = response.getHeaders();
-        headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-        headers.add("Access-Control-Allow-Credentials", ALLOWED_CREDENTIALS);
-        headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-        headers.add("Access-Control-Max-Age", MAX_AGE);
-        headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
-        headers.add("Access-Control-Expose-Headers", ALLOWED_EXPOSED_HEADERS);
-        headers.add("Access-Control-Request-Headers", ALLOWED_REQUEST_HEADERS);
-
-        headers.add("Content-Security-Policy", "upgrade-insecure-requests");
-        headers.add("Strict-Transport-Security", "max-age=1000");
-        headers.add("X-Xss-Protection", "1; mode=block");
-        headers.add("X-Frame-Options", "DENY");
-        headers.add("X-Content-Type-Options", "nosniff");
-        headers.add("Referrer-Policy", "strict-origin-when-cross-origin");
-
         if (request.getMethod() == HttpMethod.OPTIONS) {
           response.setStatusCode(HttpStatus.OK);
           return Mono.empty();
